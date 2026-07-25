2892764ebdbbd14f7a51e57b79e900cb7cf25e38
Aditimehta2026 <mythiligoud94451@gmail.com>
2026-07-25 12:32:00 -0400

    first commit

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -40,7 +40,6 @@ export class AppMenu {
                     { label: 'Withdraw', icon: 'pi pi-fw pi-arrow-down', routerLink: ['/app/page/withdraw'] },
                     { label: 'Withdrawal History', icon: 'pi pi-fw pi-history', routerLink: ['/app/page/withdrawal-history'] },
                     { label: 'Spot Trade', icon: 'pi pi-fw pi-bitcoin', routerLink: ['/app/page/spot-trade'] },
-                    { label: 'Trade History', icon: 'pi pi-fw pi-history', routerLink: ['/app/page/spot-trade-history'] },
                     { label: 'Forex Trade', icon: 'pi pi-fw pi-dollar', routerLink: ['/app/page/forex-trade'] },
                     { label: 'Events', icon: 'pi pi-fw pi-calendar', routerLink: ['/app/page/upcoming-events'] },
                     // { label: 'AI Trading', icon: 'pi pi-fw pi-chart-line', routerLink: ['/app/page/ai-trading'] },
