487e7bee18db869a28521f068dc10971b88b10f1
Aditimehta2026 <mythiligoud94451@gmail.com>
2026-07-25 13:43:33 -0400

    first commit

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -36,25 +36,13 @@ export class AppMenu {
                 label: 'Trading',
                 items: [
                     { label: 'Deposit', icon: 'pi pi-fw pi-wallet', routerLink: ['/app/page/deposit'] },
-                    // { label: 'Deposit History', icon: 'pi pi-fw pi-history', routerLink: ['/app/page/deposit-history'] },
                     { label: 'Withdraw', icon: 'pi pi-fw pi-arrow-down', routerLink: ['/app/page/withdraw'] },
                     { label: 'Withdrawal History', icon: 'pi pi-fw pi-history', routerLink: ['/app/page/withdrawal-history'] },
                     { label: 'Spot Trade', icon: 'pi pi-fw pi-bitcoin', routerLink: ['/app/page/spot-trade'] },
                     { label: 'Forex Trade', icon: 'pi pi-fw pi-dollar', routerLink: ['/app/page/forex-trade'] },
                     { label: 'Events', icon: 'pi pi-fw pi-calendar', routerLink: ['/app/page/upcoming-events'] },
-                    // { label: 'AI Trading', icon: 'pi pi-fw pi-chart-line', routerLink: ['/app/page/ai-trading'] },
                     { label: 'Loan', icon: 'pi pi-fw pi-money-bill', routerLink: ['/app/page/loan'] },
-                    { label: 'Support', icon: 'pi pi-fw pi-check-square', routerLink: ['/app/page/support'] },
-                    // { label: 'Market', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/app/page/market'] },
-
-                    // { label: 'Overlay', icon: 'pi pi-fw pi-clone', routerLink: ['/uikit/overlay'] },
-                    // { label: 'Media', icon: 'pi pi-fw pi-image', routerLink: ['/app/page/media'] },
-                    // { label: 'Menu', icon: 'pi pi-fw pi-bars', routerLink: ['/uikit/menu'] },
-                    // { label: 'Message', icon: 'pi pi-fw pi-comment', routerLink: ['/uikit/message'] },
-                    // { label: 'File', icon: 'pi pi-fw pi-file', routerLink: ['/uikit/file'] },
-                    // { label: 'Chart', icon: 'pi pi-fw pi-chart-bar', routerLink: ['/uikit/charts'] },
-                    // { label: 'Timeline', icon: 'pi pi-fw pi-calendar', routerLink: ['/uikit/timeline'] },
-                    // { label: 'Misc', icon: 'pi pi-fw pi-circle', routerLink: ['/uikit/misc'] }
+                    { label: 'Support', icon: 'pi pi-fw pi-check-square', routerLink: ['/app/page/support'] }
                 ]
             },
             {
@@ -63,7 +51,7 @@ export class AppMenu {
                 routerLink: ['/pages'],
                 items: [
                     {
-                        label: 'AI Trade',      // Sub-menu label
+                        label: 'Start AI Trading',      // Sub-menu label
                         icon: 'pi pi-fw pi-play',     // Icon to indicate starting action
                         items: [
                             {
@@ -71,45 +59,9 @@ export class AppMenu {
                                 icon: 'pi pi-fw pi-chart-line',
                                 routerLink: ['/app/page/ai-trading']
                             }
-                            // {
-                            //     label: 'Login',
-                            //     icon: 'pi pi-fw pi-sign-in',
-                            //     routerLink: ['/auth/login']
-                            // },
-                            // {
-                            //     label: 'Error',
-                            //     icon: 'pi pi-fw pi-times-circle',
-                            //     routerLink: ['/auth/error']
-                            // },
-                            // {
-                            //     label: 'Access Denied',
-                            //     icon: 'pi pi-fw pi-lock',
-                            //     routerLink: ['/auth/access']
-                            // }
 
                         ]
-                    },
-                    // {
-                    //     label: 'Landing',
-                    //     icon: 'pi pi-fw pi-globe',
-                    //     routerLink: ['/landing']
-                    // },
-
-                    // {
-                    //     label: 'Crud',
-                    //     icon: 'pi pi-fw pi-pencil',
-                    //     routerLink: ['/pages/crud']
-                    // },
-                    // {
-                    //     label: 'Not Found',
-                    //     icon: 'pi pi-fw pi-exclamation-circle',
-                    //     routerLink: ['/pages/notfound']
-                    // },
-                    // {
-                    //     label: 'Empty',
-                    //     icon: 'pi pi-fw pi-circle-off',
-                    //     routerLink: ['/pages/empty']
-                    // }
+                    }
                 ]
             },
             {
@@ -118,7 +70,7 @@ export class AppMenu {
                 routerLink: ['/pages'],
                 items: [
                     {
-                        label: 'MultiAsset Trade',
+                        label: 'Start Commodity Trading',
                         icon: 'pi pi-fw pi-globe',
                         items: [
                             {
@@ -130,67 +82,6 @@ export class AppMenu {
                     }
                 ]
             }
-            // ,{
-            //     label: 'Hierarchy',
-            //     items: [
-            //         {
-            //             label: 'Submenu 1',
-            //             icon: 'pi pi-fw pi-bookmark',
-            //             items: [
-            //                 {
-            //                     label: 'Submenu 1.1',
-            //                     icon: 'pi pi-fw pi-bookmark',
-            //                     items: [
-            //                         { label: 'Submenu 1.1.1', icon: 'pi pi-fw pi-bookmark' },
-            //                         { label: 'Submenu 1.1.2', icon: 'pi pi-fw pi-bookmark' },
-            //                         { label: 'Submenu 1.1.3', icon: 'pi pi-fw pi-bookmark' }
-            //                     ]
-            //                 },
-            //                 {
-            //                     label: 'Submenu 1.2',
-            //                     icon: 'pi pi-fw pi-bookmark',
-            //                     items: [{ label: 'Submenu 1.2.1', icon: 'pi pi-fw pi-bookmark' }]
-            //                 }
-            //             ]
-            //         },
-            //         {
-            //             label: 'Submenu 2',
-            //             icon: 'pi pi-fw pi-bookmark',
-            //             items: [
-            //                 {
-            //                     label: 'Submenu 2.1',
-            //                     icon: 'pi pi-fw pi-bookmark',
-            //                     items: [
-            //                         { label: 'Submenu 2.1.1', icon: 'pi pi-fw pi-bookmark' },
-            //                         { label: 'Submenu 2.1.2', icon: 'pi pi-fw pi-bookmark' }
-            //                     ]
-            //                 },
-            //                 {
-            //                     label: 'Submenu 2.2',
-            //                     icon: 'pi pi-fw pi-bookmark',
-            //                     items: [{ label: 'Submenu 2.2.1', icon: 'pi pi-fw pi-bookmark' }]
-            //                 }
-            //             ]
-            //         }
-            //     ]
-            // }
-            // {
-            //     label: 'Get Started',
-            //     items: [
-            //         {
-            //             label: 'Documentation',
-            //             icon: 'pi pi-fw pi-book',
-            //             routerLink: ['/documentation']
-            //         },
-            //         {
-            //             label: 'View Source',
-            //             icon: 'pi pi-fw pi-github',
-            //             // url: 'https://github.com/primefaces/sakai-ng',
-            //             // target: '_blank'
-            //             routerLink: ['/']
-            //         }
-            //     ]
-            // }
         ];
     }
     closeMenu() {
